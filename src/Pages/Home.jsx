600c608ecb193b26747959399c275cffa4edae4f
TechgicusSoftware <TechgicusAppwrite@gmail.com>
2025-04-23 18:40:00 +0530

    done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import Header from "../Components/Header";
 import { IoChevronForward } from "react-icons/io5";
 import { IoIosArrowBack } from "react-icons/io";
 import "./Home.css";
-import video1 from "../Assets/Shree Nagari III.mp4"; // Path to your uploaded video
+// import video1 from "../Assets/Shree Nagari III.mp4"; // Path to your uploaded video
 import About from "./About";
 import Yogabenefit from "./Amenities";
 import ServicesPage from "./Services";
@@ -16,29 +16,29 @@ import Projects from "./Projects";
 import Amenities from './Amenities';
 
 function Home() {
-  const slides = [
-    {
-      media: video1,
-    },
-  ];
+  // const slides = [
+  //   {
+  //     media: video1,
+  //   },
+  // ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const nextSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
-  };
+  // const nextSlide = () => {
+  //   setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
+  // };
 
-  const prevSlide = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? slides.length - 1 : prevIndex - 1
-    );
-  };
+  // const prevSlide = () => {
+  //   setCurrentIndex((prevIndex) =>
+  //     prevIndex === 0 ? slides.length - 1 : prevIndex - 1
+  //   );
+  // };
 
   // Automatically transition slides every 5 seconds
-  useEffect(() => {
-    const interval = setInterval(nextSlide, 5000); // 5000ms = 5 seconds
-    return () => clearInterval(interval); // Clear interval on component unmount
-  }, []);
+  // useEffect(() => {
+  //   const interval = setInterval(nextSlide, 5000); // 5000ms = 5 seconds
+  //   return () => clearInterval(interval); // Clear interval on component unmount
+  // }, []);
 
   return (
     <>
@@ -49,21 +49,14 @@ function Home() {
           
           <div className="slide" key={currentIndex}>
             <video
-              src={slides[currentIndex].media}
+              src="https://resallingo.sfo3.digitaloceanspaces.com/resallingo/Shree%20Nagari%20III.mp4"
               controls={false}
               autoPlay
               loop
               
               className="slider-video"
             />
-            <div className="content">
-              <h2>Shree Swami Nityanand</h2>
-              <h2>Land Developer</h2>
-              <h3>Buy Plot To Make Your Dream Home</h3>
-              <a href="#contact">
-                <button>Contact Us</button>
-              </a>
-            </div>
+           
           </div>
          
         </div>
